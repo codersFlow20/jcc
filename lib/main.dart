d3148e7aa730ab2e3c09cfa7d511b3f6ea9ee990
bhavy <bhavyukani1@gmail.com>
2023-10-11 21:57:05 +0530

    SplashScreen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jcc/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterLogo(
-      size: double.minPositive,
-    );
+    return SplashScreen();
   }
 }
