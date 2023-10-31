2f90479060ccbca7f3ff9bbfcc44b2b67aa63367
Jay <pedhadiyajay5230@gmail.com>
2023-10-31 13:00:54 +0530

    Done Some Refactor

---
+++ lib/bloc/notification/notification_bloc.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:developer' as dev;
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:jcc/repositories/notification_repository.dart';
+
+import '../../models/notification_model.dart';
+
+part 'notification_event.dart';
+
+part 'notification_state.dart';
+
+class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
+
+  bool descending = true;
+
+  NotificationBloc({required NotificationRepository notificationRepository})
+      : _notificationRepository = notificationRepository,
+        super(NotificationInitial()) {
+    on<LoadNotifications>(_onLoadNotifications);
+    on<UpdateNotification>(_onUpdateNotification);
+  }
+
+  final NotificationRepository _notificationRepository;
+  StreamSubscription? _notificationSubscription;
+
+
+
+  FutureOr<void> _onUpdateNotification(
+      UpdateNotification event, Emitter<NotificationState> emit) {
+    emit(NotificationLoaded(event.notificationList));
+  }
+
+  FutureOr<void> _onLoadNotifications(
+      LoadNotifications event, Emitter<NotificationState> emit) {
+    _notificationSubscription?.cancel();
+
+    try {
+      dev.log('state is  $descending');
+      _notificationSubscription =
+          _notificationRepository.getNotification(true).listen((list) {
+        add(UpdateNotification(list));
+      });
+    } catch (e) {
+      emit(NotificationError(e.toString()));
+    }
+  }
+
+
+
+  @override
+  void onTransition(
+      Transition<NotificationEvent, NotificationState> transition) {
+    super.onTransition(transition);
+    dev.log(transition.toString(), name: "Notification Screen");
+  }
+
+
+}
---
+++ lib/bloc/notification/notification_event.dart
@@ -0,0 +1,19 @@
+part of 'notification_bloc.dart';
+
+abstract class NotificationEvent extends Equatable {
+  const NotificationEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadNotifications extends NotificationEvent{
+
+}
+
+class UpdateNotification extends NotificationEvent {
+  const UpdateNotification(this.notificationList);
+
+  final List<NotificationModel> notificationList;
+}
+
